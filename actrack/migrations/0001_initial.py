45bb00b5a3f4132595ee3e2cee6dac96110befe8
Thomas Khyn <thomas@ksytek.com>
2014-11-25 14:31:47 +1300

    Adds Django 1.7 migrations

---
+++ actrack/migrations/0001_initial.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import gm2m.fields
+import jsonfield.fields
+import actrack.models
+import django.utils.timezone
+from django.conf import settings
+import actrack.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Action',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('actor_pk', models.CharField(max_length=255)),
+                ('verb', models.CharField(max_length=255)),
+                ('data', jsonfield.fields.JSONField(null=True, blank=True)),
+                ('timestamp', models.DateTimeField(default=django.utils.timezone.now)),
+                ('targets', gm2m.fields.GM2MField(related_name=b'actions_as_target+')),
+                ('related', gm2m.fields.GM2MField(related_name=b'actions_as_related+')),
+                ('actor_ct', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+                'ordering': ('-timestamp',),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='DeletedItem',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('description', models.CharField(max_length=255)),
+                ('ctype', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tracker',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tracked_pk', models.CharField(max_length=255, null=True)),
+                ('verbs', actrack.fields.VerbsField(max_length=1000)),
+                ('actor_only', models.BooleanField(default=True)),
+                ('last_updated', models.DateTimeField(default=django.utils.timezone.now)),
+                ('fetched_elsewhere', models.ManyToManyField(related_name='fetched+', to='actrack.Action')),
+                ('tracked_ct', models.ForeignKey(to='contenttypes.ContentType')),
+                ('user', models.ForeignKey(related_name='trackers+', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model, actrack.models.TrackerBase),
+        ),
+        migrations.CreateModel(
+            name='UnreadTracker',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('unread_actions', models.ManyToManyField(related_name='unread_in', to='actrack.Action')),
+                ('user', actrack.fields.OneToOneField(related_name='unread_actions', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
