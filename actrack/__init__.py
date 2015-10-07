591006894823faabd66743d1b0594666acae684a
Thomas Khyn <thomas@ksytek.com>
2015-10-07 14:39:08 +1300

    Fixes __init__

--- actrack/__init__.py
+++ actrack/__init__.py
@@ -1,7 +1,7 @@
 from .version import __version__, __version_info__
 
-default_app_config = 'actrack.apps.ActrackConfig'
-
 from .decorators import connect
 from .signals import log
 from .actions import track, untrack
+
+default_app_config = 'actrack.apps.ActrackConfig'
